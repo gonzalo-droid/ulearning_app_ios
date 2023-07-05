d0c4892372610026699adb84706553f1f633f063
Gonzalo López Guerrero <gonzalo.lozg@gmail.com>
2023-07-05 01:36:16 -0500

    [WIP] Login UI

---
+++ ulearning_app_ios/Controllers/Login/ULLoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ULLoginViewController.swift
+//  ulearning_app_ios
+//
+//  Created by Gonzalo López on 5/07/23.
+//
+
+import UIKit
+
+class ULLoginViewController: UIViewController {
+
+    @IBAction func doLoginButton(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
